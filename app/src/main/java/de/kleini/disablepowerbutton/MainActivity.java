1dd10b1f958a50e2622f95d809b3a3ac379bdce3
Stefan Kleini <kleini@gfbbb.de>
2020-08-06 12:12:46 +0200

    Initial commit

---
+++ app/src/main/java/de/kleini/disablepowerbutton/MainActivity.java
@@ -0,0 +1,74 @@
+package de.kleini.disablepowerbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static int REQUEST_CODE = 10101;
+    private static final String TAG = "TAG";
+
+    // Example from https://stackoverflow.com/a/55794487
+    // FIXME Doesn´t work properly on Samsung Galaxy S10
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        if (checkDrawOverlayPermission()) {
+            startService(new Intent(this, PowerButtonService.class));
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) {
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
+    public boolean checkDrawOverlayPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+
+            return true;
+        }
+        if (!Settings.canDrawOverlays(this)) {
+            /** if not construct intent to request permission */
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            /** request permission via start activity for result */
+            startActivityForResult(intent, REQUEST_CODE);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+                startService(new Intent(this, PowerButtonService.class));
+            }
+        }
+    }
+}
---
+++ app/src/main/java/de/kleini/disablepowerbutton/PowerButtonService.java
@@ -0,0 +1,81 @@
+package de.kleini.disablepowerbutton;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class PowerButtonService extends Service {
+
+    public PowerButtonService() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        LinearLayout mLinear = new LinearLayout(getApplicationContext()) {
+
+            //home or recent button
+            public void onCloseSystemDialogs(String reason) {
+                if ("globalactions".equals(reason)) {
+                    Log.i("Key", "Long press on power button");
+                    //power key pressed
+                } else if ("homekey".equals(reason)) {
+                    //home key pressed
+                } else if ("recentapss".equals(reason)) {
+                    // recent apps button clicked
+                }
+            }
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+                if (event.getKeyCode() == KeyEvent.KEYCODE_BACK
+                        || event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP
+                        || event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN
+                        || event.getKeyCode() == KeyEvent.KEYCODE_CAMERA
+                        || event.getKeyCode() == KeyEvent.KEYCODE_POWER) {
+                    Log.i("Key", "keycode " + event.getKeyCode());
+                }
+                return super.dispatchKeyEvent(event);
+            }
+        };
+
+        mLinear.setFocusable(true);
+
+        int LAYOUT_FLAG;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        View mView = LayoutInflater.from(this).inflate(R.layout.service_layout, mLinear);
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        //params
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
+        wm.addView(mView, params);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
